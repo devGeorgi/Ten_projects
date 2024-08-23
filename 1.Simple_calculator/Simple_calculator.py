fc9348fc64af087c161c5765023d71e333a51059
Georgi <georgihristov2003@gmail.com>
2024-08-23 11:26:35 +0300

    Simple Calculator done

---
+++ 1.Simple_calculator/Simple_calculator.py
@@ -0,0 +1,59 @@
+# Addition
+def addition(x, y):
+    return x + y
+
+# Subtraction
+def subtraction(x, y):
+    return x - y
+
+# Multiplication
+def multiplication(x, y):
+    return x * y
+
+# Division
+def division(x, y):
+    return x / y
+
+# Exponentiation
+def exponentiation(x, y):
+    return x ** y
+
+while True :
+    # Get the action from the user
+    action = input("What action do you want to do? (addition, subtraction, multiplication, division): ")
+
+    # Perform the selected operation
+    if action == "addition":
+        # Get the numbers from the user
+        x = float(input("Give me the first number: "))
+        y = float(input("Give me the second number: "))
+        print("Result:", addition(x, y))
+
+    elif action == "subtraction":
+        # Get the numbers from the user
+        x = float(input("Give me the first number: "))
+        y = float(input("Give me the second number: "))
+        print("Result:", subtraction(x, y))
+
+    elif action == "multiplication":
+        # Get the numbers from the user
+        x = float(input("Give me the first number: "))
+        y = float(input("Give me the second number: "))
+        print("Result:", multiplication(x, y))
+
+    elif action == "division":
+        # Get the numbers from the user
+        x = float(input("Give me the first number: "))
+        y = float(input("Give me the second number: "))
+        if y != 0:
+            print("Result:", division(x, y))
+        else:
+            print("Error: Division by zero is not allowed.")
+
+    elif action == "exponentiation":
+        x = float(input("Give me the base number: "))
+        y = float(input("Give me the exponent: "))
+        print("Result:", exponentiation(x, y))
+
+    else :
+        print("Invalid action")
